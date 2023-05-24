d8cc2eb8acb5c524f8cbf2c0499d8b2e8eadb796
sani-coder <saneeppp@gmail.com>
2023-05-24 12:31:05 +0530

    Completed Successfully

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://api.airtable.com/v0/appIaGZmLz0PvosOP/Table1/",
+        {
+          headers: {
+            Authorization:
+              "Bearer pato0xvMppECQkQzX.8c16c36c57c3bdd3c421b622591f2a1957243c2e90286e1b4bf29ba9fb21a4a7",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error fetching data from Airtable");
+      }
+
+      const responseData = await response.json();
+      let details = responseData.records;
+      setData(details);
+      data.map((item) => {
+        console.log(item.fields);
+      });
+    } catch (error) {
+      console.error("Error fetching data from Airtable:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <table>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+
+          {data.map((item) => (
+            <tr>
+              <td>{item.fields.Name}</td>
+              <td>{item.fields.Email}</td>
+              <td>{item.fields.Phone}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
